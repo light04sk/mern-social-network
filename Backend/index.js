b89df37aff80d7167061c67a9e4981f75e8d0f9a
light04sk <skmr39630@gmail.com>
2026-02-13 23:55:42 +0530

    integrate routes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,27 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 
+import postRoutes from "./routes/postRoute.js";
+import userRoutes from "./routes/userRoute.js";
+import notificationRoutes from "./routes/notificationRoute.js";
+import commentRoutes from "./routes/commentRoute.js";
+import connectionRoutes from "./routes/connectionRoute.js";
+import conversationRoutes from "./routes/conversationRoute.js";
+import messageRoutes from "./routes/messageRoute.js";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("uploads"));
+app.use(postRoutes);
+app.use(userRoutes);
+app.use(notificationRoutes);
+app.use(commentRoutes);
+app.use(connectionRoutes);
+app.use(conversationRoutes);
+app.use(messageRoutes);
 
 const port = process.env.PORT || 4000;
 
