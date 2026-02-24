dc12a9c05fa74cfa572cafd26bcfb1351a969d73
light04sk <skmr39630@gmail.com>
2026-02-25 00:02:03 +0530

    add notification route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Network from "./pages/Network";
 import Message from "./pages/Message";
 import Profile from "./pages/Profile";
 import ProfilePosts from "./pages/ProfilePosts";
+import Notifications from "./pages/Notifications";
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
           <Route path="/feed" element={<Feed />} />
           <Route path="/network" element={<Network />} />
           <Route path="/message" element={<Message />} />
+          <Route path="/notification" element={<Notifications />} />
           <Route path="/profile/:id" element={<Profile />} />
           <Route path="/profile/:id/posts" element={<ProfilePosts />} />
         </Route>
