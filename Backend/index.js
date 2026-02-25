b55517ceadcbb242e7c8d0e770d2f2a436c9c83c
light04sk <skmr39630@gmail.com>
2026-02-25 23:36:36 +0530

    feat: add CORS middleware and install cors package

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import postRoutes from "./routes/postRoute.js";
 import userRoutes from "./routes/userRoute.js";
@@ -14,9 +15,18 @@ import messageRoutes from "./routes/messageRoute.js";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("uploads"));
+
 app.use(postRoutes);
 app.use(userRoutes);
 app.use(notificationRoutes);
